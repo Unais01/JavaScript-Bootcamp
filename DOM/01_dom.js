586db8606759ce0719d8cfe294385eae7aee5465
Unais <unaissk1234@gmail.com>
2024-03-06 12:41:56 +0530

    learned selectors

--- DOM/01_dom.js
+++ DOM/01_dom.js
@@ -1,1 +1,37 @@
-ECHO is on.
+
+//Elements Selectors
+
+
+// /*  getElementById*/
+// const heading = document.getElementById('heading');
+// heading.style.color = 'purple';
+
+// const list = document.getElementById('list');
+// list.style.backgroundColor='purple';
+
+
+/*  getElementsByClassName*/
+// const listItem = document.getElementsByClassName('list-item');
+
+//getElementsByTagName
+// const li = document.getElementsByTagName('li');
+
+//
+// const li=document.getElementsByName('li');
+
+// console.log(li);
+
+const heading = document.querySelector('#ctn #heading');
+heading.style.color='red'
+
+// const li = document.querySelector('.list-item');
+// li.style.background='purple';
+let color = ['red','green','yellow','purple','pink','black','white']
+const li = document.querySelectorAll('.list-item');
+li.forEach((liItem)=>{
+    let generate = Math.floor(Math.random()*7);
+    liItem.style.color=color[generate]
+    liItem.innerHTML="shaikh Unais";
+})
+
+                                 
